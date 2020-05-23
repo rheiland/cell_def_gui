8c769af353e847a61f5df59af5a7abd016b43150
Randy Heiland <heiland@indiana.edu>
2020-05-23 07:32:28 -0400

    add new

---
+++ cell_def_mockup1.py
@@ -0,0 +1,108 @@
+ 
+# This file is auto-generated from a Python script that parses a PhysiCell configuration (.xml) file.
+#
+# Edit at your own risk.
+#
+import os
+from ipywidgets import Label,Text,Checkbox,Button,HBox,VBox,FloatText,IntText,BoundedIntText,BoundedFloatText,Layout,Box,Dropdown
+    
+class CellsDefTab(object):
+
+    def __init__(self):
+        
+        micron_units = Label('micron')   # use "option m" (Mac, for micro symbol)
+
+        constWidth = '180px'
+        tab_height = '500px'
+        stepsize = 10
+
+        #style = {'description_width': '250px'}
+        style = {'description_width': '25%'}
+        layout = {'width': '400px'}
+
+        name_button_layout={'width':'25%'}
+        widget_layout = {'width': '15%'}
+        units_button_layout ={'width':'15%'}
+        desc_button_layout={'width':'45%'}
+        divider_button_layout={'width':'40%'}
+
+        self.parent_name = Text(value='None',placeholder='Type something',description='Parent:',disabled=True)
+
+        self.cell_type = Dropdown(description='Cell type:',)
+        self.cell_type.style = {'description_width': '%sch' % str(len(self.cell_type.description) + 1)}
+
+
+        self.cell_type_dict = {}
+        self.cell_type_dict['default'] = 'default'
+        self.cell_type_dict['lung epithelium'] = 'lung epithelium'
+        self.cell_type_dict['immune'] = 'immune'
+        self.cell_type_dict['CD8 Tcell'] = 'CD8 Tcell'
+        self.cell_type_dict['macrophage'] = 'macrophage'
+        self.cell_type_dict['neutrophil'] = 'neutrophil'
+        self.cell_type.options = self.cell_type_dict
+
+        units_button1 = Button(description='', disabled=True, layout=units_button_layout) 
+        units_button1.style.button_color = 'lightgreen'
+        units_button2 = Button(description='', disabled=True, layout=units_button_layout) 
+        units_button2.style.button_color = 'tan'
+        units_button3 = Button(description='', disabled=True, layout=units_button_layout) 
+        units_button3.style.button_color = 'lightgreen'
+        units_button4 = Button(description='', disabled=True, layout=units_button_layout) 
+        units_button4.style.button_color = 'tan'
+        units_button5 = Button(description='', disabled=True, layout=units_button_layout) 
+        units_button5.style.button_color = 'lightgreen'
+        units_button6 = Button(description='', disabled=True, layout=units_button_layout) 
+        units_button6.style.button_color = 'tan'
+
+        desc_button1 = Button(description='', disabled=True, layout=desc_button_layout) 
+        desc_button1.style.button_color = 'lightgreen'
+        desc_button2 = Button(description='', disabled=True, layout=desc_button_layout) 
+        desc_button2.style.button_color = 'tan'
+        desc_button3 = Button(description='', disabled=True, layout=desc_button_layout) 
+        desc_button3.style.button_color = 'lightgreen'
+        desc_button4 = Button(description='', disabled=True, layout=desc_button_layout) 
+        desc_button4.style.button_color = 'tan'
+        desc_button5 = Button(description='', disabled=True, layout=desc_button_layout) 
+        desc_button5.style.button_color = 'lightgreen'
+        desc_button6 = Button(description='', disabled=True, layout=desc_button_layout) 
+        desc_button6.style.button_color = 'tan'
+
+
+        box_layout = Layout(display='flex', flex_flow='row', align_items='stretch', width='100%')
+
+        item_layout = Layout(height='500px', min_width='40px')
+        items = [Button(layout=item_layout, description=str(i), button_style='warning') for i in range(40)]
+        box_layout = Layout(overflow='scroll hidden',
+                    border='3px solid black',
+                    width='',
+                    height='400px',
+                    flex_flow='row',
+                    display='flex')
+        carousel = Box(children=items, layout=box_layout)
+       VBox([Label('Scroll horizontally:'), carousel])
+
+        self.tab = VBox([
+          self.cell_type, 
+          carousel,
+        ])
+
+    # Populate the GUI widgets with values from the XML
+    def fill_gui(self, xml_root):
+        uep = xml_root.find('.//microenvironment_setup')  # find unique entry point
+        vp = []   # pointers to <variable> nodes
+        if uep:
+            for var in uep.findall('variable'):
+                vp.append(var)
+
+        uep = xml_root.find('.//cell_definitions')  # find unique entry point
+
+
+    # Read values from the GUI widgets to enable editing XML
+    def fill_xml(self, xml_root):
+        uep = xml_root.find('.//microenvironment_setup')  # find unique entry point
+        vp = []   # pointers to <variable> nodes
+        if uep:
+            for var in uep.findall('variable'):
+                vp.append(var)
+
+        uep = xml_root.find('.//cell_definitions')  # find unique entry point
